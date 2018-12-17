c0da61ae33f3227ab81f5d91a0901551b4070c1d
Nick Ellis <nicholas.w.ellis@gmail.com>
2018-12-17 11:28:49 -0600

    Add status page for exception handling

--- src/main/kotlin/me/nickellis/App.kt
+++ src/main/kotlin/me/nickellis/App.kt
@@ -3,20 +3,31 @@ package me.nickellis
 import io.ktor.application.*
 import io.ktor.features.*
 import io.ktor.html.respondHtml
+import io.ktor.http.HttpStatusCode
 import io.ktor.http.content.file
 import io.ktor.http.content.files
 import io.ktor.http.content.resources
 import io.ktor.http.content.static
+import io.ktor.response.respond
+import io.ktor.response.respondWrite
 import io.ktor.routing.*
 import kotlinx.html.body
 import kotlinx.html.head
 import kotlinx.html.link
 import kotlinx.html.p
+import me.nickellis.routes.root
 
 
 fun Application.main() {
   install(DefaultHeaders)
   install(CallLogging)
+
+  install(StatusPages) {
+    exception<Throwable> {
+      call.respond(HttpStatusCode.InternalServerError)
+    }
+  }
+
   routing {
     get("/") {
       call.respondHtml {
