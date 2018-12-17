b5f002712b140281d8d72e24eaed555b99a2d557
Nick Ellis <nicholas.w.ellis@gmail.com>
2018-12-17 11:21:17 -0600

    Add route for static files

--- src/main/kotlin/me/nickellis/App.kt
+++ src/main/kotlin/me/nickellis/App.kt
@@ -2,16 +2,39 @@ package me.nickellis
 
 import io.ktor.application.*
 import io.ktor.features.*
-import io.ktor.http.*
-import io.ktor.response.*
+import io.ktor.html.respondHtml
+import io.ktor.http.content.file
+import io.ktor.http.content.files
+import io.ktor.http.content.resources
+import io.ktor.http.content.static
 import io.ktor.routing.*
+import kotlinx.html.body
+import kotlinx.html.head
+import kotlinx.html.link
+import kotlinx.html.p
+
 
 fun Application.main() {
   install(DefaultHeaders)
   install(CallLogging)
   routing {
     get("/") {
-      call.respondText("Hello World!", ContentType.Text.Html)
+      call.respondHtml {
+        head {
+          link(rel = "stylesheet", href = "/static/skeleton.css")
+          link(rel = "stylesheet", href = "/static/normalize.css")
+        }
+        body {
+          p {
+            text("Hello World")
+          }
+        }
+      }
+    }
+
+    static("/static") {
+      resources("static/styles")
+      resources("static/images")
     }
   }
 }
