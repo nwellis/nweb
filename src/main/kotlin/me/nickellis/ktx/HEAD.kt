3fb34afa71fd7c309c30a2869762e626e48fff25
Nick Ellis <nicholas.w.ellis@gmail.com>
2018-12-17 12:33:41 -0600

    Add default headers and swap css to Bulma

---
+++ src/main/kotlin/me/nickellis/ktx/HEAD.kt
@@ -0,0 +1,21 @@
+package me.nickellis.ktx
+
+import kotlinx.html.HEAD
+import kotlinx.html.link
+import kotlinx.html.meta
+import kotlinx.html.script
+
+fun HEAD.defaultHeaders() {
+  link(rel = "icon", href = "/static/favicon.png")
+  meta(charset = "utf-8")
+  responsiveViewport()
+  styles()
+  fontAwesomeScript()
+}
+
+fun HEAD.styles() = link(rel = "stylesheet", href = "/static/bulma.min.css")
+
+fun HEAD.responsiveViewport() = meta(name = "viewport", content = "width=device-width, initial-scale=1")
+
+fun HEAD.fontAwesomeScript() =
+    script(type = "text/javascript", src = "https://use.fontawesome.com/releases/v5.3.1/js/all.js") { defer=true }
--- src/main/kotlin/me/nickellis/routes/Root.kt
+++ src/main/kotlin/me/nickellis/routes/Root.kt
@@ -4,17 +4,14 @@ import io.ktor.application.call
 import io.ktor.html.respondHtml
 import io.ktor.routing.Routing
 import io.ktor.routing.get
-import kotlinx.html.body
-import kotlinx.html.head
-import kotlinx.html.link
-import kotlinx.html.p
+import kotlinx.html.*
+import me.nickellis.ktx.defaultHeaders
 
 fun Routing.root() {
   get("/") {
     call.respondHtml {
       head {
-        link(rel = "stylesheet", href = "/static/skeleton.css")
-        link(rel = "stylesheet", href = "/static/normalize.css")
+        defaultHeaders()
       }
       body {
         p {
