14e739b2a594a9cab15eef58573cf28171b790be
nwellis <nicholas.w.ellis@gmail.com>
2023-04-08 16:45:13 -0500

    fix: restore to original state

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
 import image from "@astrojs/image";
 import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
 
 const CWD = process.cwd();
 const idIncludes = (prefix, ...checks) => {
@@ -20,9 +21,6 @@ const merge =
 const ChunkNameAndCheck = {
   // Add a trailing slash to match the folder name exactly
 
-  // NODE_MODULES
-  "vendor-lodash": nodeModuleIncludes("lodash"),
-
   "vendor-phaser-core": merge(
     nodeModuleIncludes("phaser/dist"),
     srcIncludes("components/game/demo", "components/game/common")
@@ -39,8 +37,10 @@ const ChunkNameAndCheck = {
   "vendor-phaser-types": nodeModuleIncludes("phaser/types"),
   "vendor-phaser-misc": nodeModuleIncludes("phaser"),
 
+  "vendor-lodash": nodeModuleIncludes("lodash"),
   // "vendor": nodeModuleIncludes(""),
 };
+
 const ManualChunks = Object.entries(ChunkNameAndCheck).map(([name, check]) => ({
   name,
   check,
@@ -50,8 +50,11 @@ const ManualChunks = Object.entries(ChunkNameAndCheck).map(([name, check]) => ({
 export default defineConfig({
   integrations: [
     image(),
+    react(),
     tailwind({
-      config: { applyBaseStyles: false },
+      config: {
+        applyBaseStyles: false,
+      },
     }),
     // disablePreload("vendor-phaser"),
   ],
@@ -65,23 +68,9 @@ export default defineConfig({
         },
       },
     },
-
-    modulePreload: {
-      polyfill: true,
-      resolveDependencies: (filename, deps, { hostId, hostType }) => {
-        import("fs/promises").then(({ writeFile }) => {
-          // All the chunk logs push this out of view, so save as files
-          writeFile(
-            `./DEBUG_RES_${filename.split("/").at(1)}.json`,
-            JSON.stringify({ filename, hostId, hostType, deps }, null, 2)
-          );
-        });
-        return deps.filter(
-          (dep) => !/^vendor-phaser/i.test(dep.replace("static/", ""))
-        );
-      },
-    },
   },
 
-  server: { port: 3003 },
+  server: {
+    port: 3003,
+  },
 });
