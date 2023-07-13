c618c62ac94d319fbb34d43bfb6ae736bdc730ff
njord2 <njord@4-cube.io>
2023-07-13 13:09:46 -0500

    chore: misc changes

--- src/components/game/common/BaseScene.ts
+++ src/components/game/common/BaseScene.ts
@@ -4,4 +4,13 @@ export default class BaseScene extends Phaser.Scene {
   constructor(config?: string | Phaser.Types.Scenes.SettingsConfig) {
     super(config);
   }
+
+  preload() {
+    /**
+     * Phaser will load things relative to its current URL, so this is required to reference the
+     * public folder! For example {domain}/home will reference {domain}/assets from the public
+     * folder, but if you have {domain}/home/page2 it will try reference it like {domain}/home/assets.
+     */
+    this.load.setBaseURL(window.location.origin);
+  }
 }
